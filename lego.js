e115740e76891c94aa34d34d689ca0d152b7cc68
askeip <paparazzi730@gmail.com>
2016-11-01 21:44:41 +0500

    select, filterIn, format released

--- lego.js
+++ lego.js
@@ -4,7 +4,7 @@
  * Сделано задание на звездочку
  * Реализованы методы or и and
  */
-exports.isStar = true;
+exports.isStar = false;
 
 /**
  * Запрос к коллекции
@@ -16,24 +16,66 @@ exports.query = function (collection) {
     return collection;
 };
 
+function convertForQuery(func, params) {
+    return { func: func,
+    params: params };
+}
+
 /**
  * Выбор полей
  * @params {...String}
+ * @returns {Object}
  */
 exports.select = function () {
-    return;
+    return convertForQuery(selectFields, [].slice.call(arguments));
 };
+
+/**
+ * @this mapFriends
+ * @param {Object} friend
+ * @returns {Object}
+ */
+function mapFriends(friend) {
+    var mappedFriend = {};
+    for (var param in this) {
+        if (this.hasOwnProperty(param) && friend.hasOwnProperty(this[param])) {
+            mappedFriend[this[param]] = friend[this[param]];
+        }
+    }
+
+    return mappedFriend;
+}
+
+function selectFields(friends, params) {
+    return friends.map(mapFriends, params);
+}
 
 /**
  * Фильтрация поля по массиву значений
  * @param {String} property – Свойство для фильтрации
  * @param {Array} values – Доступные значения
+ * @returns {Object}
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
-
-    return;
+    return convertForQuery(filterFriends, [property, values]);
 };
+
+/**
+ * @this formatFriendsField
+ * @param {Object} friend
+ * @returns {Object}
+ */
+function filterInFilter(friend) {
+    for (var filter in this[1]) {
+        if (friend[this[0]] === filter) {
+            return friend;
+        }
+    }
+}
+
+function filterFriends(friends, params) {
+    return friends.filter(filterInFilter, params);
+}
 
 /**
  * Сортировка коллекции по полю
@@ -50,12 +92,40 @@ exports.sortBy = function (property, order) {
  * Форматирование поля
  * @param {String} property – Свойство для фильтрации
  * @param {Function} formatter – Функция для форматирования
+ * @returns {Object}
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
-
-    return;
+    return convertForQuery(formatField, [property, formatter]);
 };
+
+function clone(obj) {
+    var copy = obj.constructor();
+    for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) {
+            copy[attr] = obj[attr];
+        }
+    }
+
+    return copy;
+}
+
+/**
+ * @this formatFriendsField
+ * @param {Object} friend
+ * @returns {Object}
+ */
+function formatFriendsField(friend) {
+    var formattedFriend = clone(friend);
+    if (formattedFriend.hasOwnProperty(this[0])) {
+        clone[this[0]] = this[1](formattedFriend[this[0]]);
+    }
+
+    return formattedFriend;
+}
+
+function formatField(friends, params) {
+    return friends.map(formatFriendsField, params);
+}
 
 /**
  * Ограничение количества элементов в коллекции
